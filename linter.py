899d1e20d777b068cd2f4d7ac8065d6775db619a
Nikita Gryzlov <nixel2007@gmail.com>
2016-05-18 10:44:02 +0300

    Добавлено свойство linterEntryPoint. Переработано свойство lintOtherExtensions
    https://github.com/xDrivenDevelopment/1c-syntax/issues/158

--- linter.py
+++ linter.py
@@ -13,6 +13,24 @@ class OneScriptLint(Linter):
     comment_re = r'\s*//'
 
     def run(self, cmd, code):
-        lintBSLFiles = sublime.load_settings('Language 1C (BSL).sublime-settings').get('lintBSLFiles')
-        if not (self.filename.endswith('.bsl') and not lintBSLFiles):
-            return self.communicate(cmd, code)
+        lintOtherExtensions = sublime.load_settings('Language 1C (BSL).sublime-settings').get('lintOtherExtensions')
+        linterEntryPoint = sublime.load_settings('Language 1C (BSL).sublime-settings').get('linterEntryPoint')
+        filePath = self.filename
+        arrFilePath = filePath.split('.')
+        if arrFilePath.length == 0
+            return
+        extension = arrFilePath[arrFilePath.length - 1];
+        if extension != 'os' && !lintOtherExtensions.include(extension)
+            return
+
+        if linterEntryPoint
+            project_path = ''
+            projectPaths = sublime.active_window().folders()
+            for projectPath in projectPaths
+                if filePath.indexOf(projectPath) > -1
+                    project_path = projectPath;
+                    break
+
+            cmd = cmd + " -env=" + project_path + path.sep + linterEntryPoint
+        
+        return self.communicate(cmd, code)
